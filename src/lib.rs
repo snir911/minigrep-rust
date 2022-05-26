ce82a23cf3d6a51860c8c84f1bda6c9532937aeb
Snir Sheriber <ssheribe@redhat.com>
2022-05-26 13:36:35 +0300

    completed first version

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
+use std::env;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -12,18 +14,74 @@ impl Config {
             return Err("not enough arguments"); // why it doesn't work without a return?
         }
 
-        let query = args[1].clone();
+        let query = args[1].clone(); // avoid clone
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename }) // why i can't retun args*].clone?
+        Ok(Config { query, filename, case_sensitive }) // why i can't retun args*].clone dirctly?
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("TXT:\n{}", contents);
+    let results: Vec<&str>;
+    if config.case_sensitive { // let results = if config.case_sensitive doesn't work for some reason
+        results = search(&config.query, &contents);
+    } else {
+        results = searchi(&config.query, &contents);
+    };
+    for line in results {
+        println!("{}", line);
+    }
 
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query){
+            res.push(line);
+        }
+    }
+    res
+}
+
+pub fn searchi<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()){
+            res.push(line);
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_senstitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Duct tape
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search (query, contents));
+    }
+    
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Duct tape
+Pick three.
+Trust me";
+        assert_eq!(vec!["Rust:", "Trust me"], searchi (query, contents));
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem with parsing args: {}", err );
+        eprintln!("Problem with parsing args: {}", err );
         process::exit(1);
     });
 
-    println!("Grepping for \"{}\" in the \"{}\" file", config.query, config.filename); 
-
     if let Err(e) = run(config) { //instesad of unwrap_* as we don't need to unwrap
-        println!("failed to read file: {}", e);
+        eprintln!("failed to read file: {}", e);
         process::exit(2);
     }
 }
